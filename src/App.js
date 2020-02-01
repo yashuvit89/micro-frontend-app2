e92a398d017376fd4e1f79e96d92c1f9468bf581
P S L Yaswanth <yashu.vit@gmail.com>
2020-02-01 23:26:13 +0530

    basic integration micro-frontends

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import styled, { keyframes } from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>App 2</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const pulse = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppLogo = styled.img.attrs({
+  src: logo
+})`
+  height: 30vmin;
+  pointer-events: none;
+  animation: ${pulse} infinite 10s linear;
+`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      numberOfLogos: 1
+    };
+  }
+  render() {
+    const Logos = [];
+    for (let i = 0; i < this.state.numberOfLogos; i++) {
+      Logos.push(<AppLogo></AppLogo>);
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="Logos">{Logos}</div>
+          <p>App 2</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
