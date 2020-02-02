8ffc0f70c453690841bd7135d3b4c2e7423128b9
P S L Yaswanth <yashu.vit@gmail.com>
2020-02-02 22:42:53 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import styled, { keyframes } from "styled-components";
@@ -15,32 +15,46 @@ const pulse = keyframes`
 const AppLogo = styled.img.attrs({
   src: logo
 })`
-  height: 30vmin;
+  height: 25vmin;
   pointer-events: none;
-  animation: ${pulse} infinite 10s linear;
+  animation: ${pulse} infinite ${props => props.count}s linear;
 `;
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      numberOfLogos: 2
-    };
-  }
-  render() {
-    const Logos = [];
-    for (let i = 0; i < this.state.numberOfLogos; i++) {
-      Logos.push(<AppLogo></AppLogo>);
-    }
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div className="Logos">{Logos}</div>
-          <p>App 2</p>
-        </header>
-      </div>
-    );
-  }
-}
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => (props.primary ? "palevioletred" : "white")};
+  color: ${props => (props.primary ? "white" : "palevioletred")};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
 
-export default App;
+export default function App(props) {
+  // let { path, url } = useRouteMatch();
+  const [count, setCount] = useState(20);
+
+  const Logos = [];
+  for (let i = 0; i < 2; i++) {
+    Logos.push(<AppLogo count={count}></AppLogo>);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Logos">{Logos}</div>
+        <div className="AppDetails">
+          <Button onClick={() => setCount(count + 5)}>-</Button>
+          <p>App 2</p>
+          <Button
+            primary
+            onClick={() => setCount(count - 5 < 0.5 ? 0.5 : count - 5)}
+          >
+            +
+          </Button>
+        </div>
+      </header>
+    </div>
+  );
+}
